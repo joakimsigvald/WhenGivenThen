baa062ae890635e310ca5820060e2a61b3a47ce5
Joakim Sigvald <joakim.sigvald@adlibris.com>
2023-07-02 06:29:40 +0200

    More assertions

--- Core/Assertions.cs
+++ Core/Assertions.cs
@@ -11,12 +11,15 @@ public static class Assertions
     public static void IsNotNull(this object actual) => Assert.NotNull(actual);
     public static void IsNull(this object actual) => Assert.Null(actual);
     public static void IsSameAs(this object actual, object expected) => Assert.Same(expected, actual);
-    public static void IsEmpty<TResult>(this IEnumerable<TResult> actual) => Assert.Empty(actual);
-    public static void IsNotEmpty<TResult>(this IEnumerable<TResult> actual) => Assert.Empty(actual);
-    public static void IsOne<TResult>(this IEnumerable<TResult> actual) => Assert.Single(actual);
-    public static void Counts<TResult>(this IEnumerable<TResult> actual, int expected) => actual.Count().Is(expected);
-    public static void IsLast<TResult>(this IEnumerable<TResult> actual, TResult item) => actual.Last().IsSameAs(item);
-    public static void Each<TResult>(this IEnumerable<TResult> actual, Action<TResult> assert)
+    public static void IsEmpty<TItem>(this IEnumerable<TItem> actual) => Assert.Empty(actual);
+    public static void IsNotEmpty<TItem>(this IEnumerable<TItem> actual) => Assert.Empty(actual);
+    public static void IsOne<TItem>(this IEnumerable<TItem> actual) => Assert.Single(actual);
+    public static void IsOne<TItem>(this IEnumerable<TItem> actual, Func<TItem, bool> assert) 
+        => assert(Assert.Single(actual));
+
+    public static void Counts<TItem>(this IEnumerable<TItem> actual, int expected) => actual.Count().Is(expected);
+    public static void IsLast<TItem>(this IEnumerable<TItem> actual, TItem item) => actual.Last().IsSameAs(item);
+    public static void Each<TItem>(this IEnumerable<TItem> actual, Action<TItem> assert)
         => actual.ToList().ForEach(assert);
     public static void Each<TResult>(this IEnumerable<TResult> actual, Action<TResult, int> assert)
         => actual.Select((it, i) => (it, i)).ToList().ForEach(t => assert(t.it, t.i));
