bc89d0df424820f531fd02e5a36b5cbab3600ed9
Joakim Sigvald <joakim.sigvald@adlibris.com>
2023-07-08 09:37:11 +0200

    Implemented qualified Is for struct types

--- Core/Assertions.cs
+++ Core/Assertions.cs
@@ -10,8 +10,45 @@ public static class Assertions
     /// <summary>
     /// Xunit.Assert.Equal
     /// </summary>
-    public static void Is<TValue>(this TValue actual, TValue expected) where TValue : struct 
-        => Assert.Equal(expected, actual);
+    public static void Is(this decimal actual, decimal expected, int precision)
+        => Assert.Equal(expected, actual, precision);
+
+    /// <summary>
+    /// Xunit.Assert.Equal
+    /// </summary>
+    public static void Is(this double actual, double expected, int precision)
+        => Assert.Equal(expected, actual, precision);
+
+    /// <summary>
+    /// Xunit.Assert.Equal
+    /// </summary>
+    public static void Is(this DateTime actual, DateTime expected, TimeSpan precision)
+        => Assert.Equal(expected, actual, precision);
+
+    /// <summary>
+    /// Xunit.Assert.Equal
+    /// </summary>
+    public static void Is(this double actual, double expected, double tolerance)
+        => Assert.Equal(expected, actual, tolerance);
+
+    /// <summary>
+    /// Xunit.Assert.Equal
+    /// </summary>
+    public static void Is(this float actual, float expected, double tolerance)
+        => Assert.Equal(expected, actual, tolerance);
+
+    /// <summary>
+    /// Xunit.Assert.Equal
+    /// </summary>
+    public static void Is(this string actual, 
+        string expected, bool ignoreCase, bool ignoreLineEndingDifferences, bool ignoreWhiteSpaceDifferences)
+        => Assert.Equal(expected, actual, ignoreCase, ignoreLineEndingDifferences, ignoreWhiteSpaceDifferences);
+
+    /// <summary>
+    /// Xunit.Assert.Equal
+    /// </summary>
+    public static void Is(this double actual, double expected, int precision, MidpointRounding rounding)
+        => Assert.Equal(expected, actual, precision, rounding);
 
     /// <summary>
     /// Xunit.Assert.NotEqual
